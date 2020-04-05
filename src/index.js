d954cb833229a6ff1708f894668d0ba7d8efa4eb
lyl <695498118@qq.com>
2020-04-05 23:40:50 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+function One(params) {
+    return (
+        <div>
+            one
+        </div>
+    )    
+}
+
+function Tow(obj) {
+    return (
+        <div>
+            tow
+        </div>
+    )    
+}
+function getSate() {
+    
+}
+function App() {
+    return (
+        <div>
+            <One></One>
+        </div>
+    )    
+}
