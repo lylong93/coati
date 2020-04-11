898e213f85dc90ee3b375cb77a89598e8381af3c
lyl <695498118@qq.com>
2020-04-11 23:42:24 +0800

    :art: add acorn

--- src/ape/index.js
+++ src/ape/index.js
@@ -1,5 +1,5 @@
 import Parser from './src/parser'
 import test from './src/test'
-let code = 'const a = 123'
-console.log(test)
+// let code = 'const a = 123'
+// console.log(test)
 new Parser(code).parse()
--- src/ape/src/parser.js
+++ src/ape/src/parser.js
@@ -1,10 +1,14 @@
+// import acorn from 'acorn'
+const acorn = requier('acorn')
 class Parser {
     constructor (input) {
         this.input =  input
         this.step = 0
     }
     parse() {
-        
+        let code = 'const num =1 '
+        let sta = acorn.parse(code)
+        console.log(sta)
     }
 }
 export default Parser
